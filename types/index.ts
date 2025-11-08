357bf1910b9ec73961be0ab321491c151d8dd054
lamodots <lamodots@gmail.com>
2025-11-08 12:16:29 -0800

    fixes broken types

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,10 @@
-import { SVGProps } from "react";
+import { SVGProps } from 'react';
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
+	size?: number;
 };
+
+export interface Faq {
+	answer: string;
+	question: string;
+}
