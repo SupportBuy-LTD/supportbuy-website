f968bf7955305b5797579cd38a199626ef6fe3b8
lamodots <lamodots@gmail.com>
2026-04-28 01:04:35 +0100

    connect url

--- app/business/_components/BusinessTopNavBar.tsx
+++ app/business/_components/BusinessTopNavBar.tsx
@@ -1,56 +1,56 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import ButtonLink from '@/components/shared/ButtonLink';
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import ButtonLink from "@/components/shared/ButtonLink";
 
 function BusinessTopNavBar() {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<nav className='h-20 md:px-10 lg:px-20 bg-sb-white-two fixed z-50 lg:flex justify-between items-center w-full shadow-[0px_1px_4px_0px_#0C0C0D0D] hidden'>
-			<div className='flex space-x-2'>
-				<ButtonLink
-					href='.'
-					className={`flex items-center justify-center  text-base text-sb-dark-one  font-bold  h-10 px-3  rounded-[48px] ${
-						pathname === '/'
-							? 'bg-sb-secondary-300 text-sb-primary flex items-center justify-center h-10'
-							: ''
-					}`}
-				>
-					Personal
-				</ButtonLink>
-				<ButtonLink
-					href='/business'
-					className={`flex items-center justify-center  text-base text-sb-dark-one  font-bold   h-10 px-3  rounded-[48px] ${
-						pathname === '/business'
-							? 'bg-sb-secondary-300 text-sb-primary flex items-center justify-center h-10'
-							: ''
-					}`}
-				>
-					Business
-				</ButtonLink>
-			</div>
-			<div className='absolute left-1/2 transform -translate-x-1/2'>
-				<Image src='/sb_logo.svg' height={48} width={155} alt='sb logo' />
-			</div>
-			<div className='flex space-x-2'>
-				<ButtonLink
-					href='.'
-					className=' flex text-base text-white font-bold justify-center items-center bg-sb-primary h-[34px] px-3.5 pb-1  rounded-[40px]'
-				>
-					Log in
-				</ButtonLink>
-				<ButtonLink
-					href='/business'
-					className='flex text-base text-sb-primary  justify-center items-center  bg-sb-secondary h-[34px]  px-3.5  pb-1 rounded-[40px]'
-				>
-					List Your Business
-				</ButtonLink>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="h-20 md:px-10 lg:px-20 bg-sb-white-two fixed z-50 lg:flex justify-between items-center w-full shadow-[0px_1px_4px_0px_#0C0C0D0D] hidden">
+      <div className="flex space-x-2">
+        <ButtonLink
+          href="."
+          className={`flex items-center justify-center  text-base text-sb-dark-one  font-bold  h-10 px-3  rounded-[48px] ${
+            pathname === "/"
+              ? "bg-sb-secondary-300 text-sb-primary flex items-center justify-center h-10"
+              : ""
+          }`}
+        >
+          Personal
+        </ButtonLink>
+        <ButtonLink
+          href="/business"
+          className={`flex items-center justify-center  text-base text-sb-dark-one  font-bold   h-10 px-3  rounded-[48px] ${
+            pathname === "/business"
+              ? "bg-sb-secondary-300 text-sb-primary flex items-center justify-center h-10"
+              : ""
+          }`}
+        >
+          Business
+        </ButtonLink>
+      </div>
+      <div className="absolute left-1/2 transform -translate-x-1/2">
+        <Image src="/sb_logo.svg" height={48} width={155} alt="sb logo" />
+      </div>
+      <div className="flex space-x-2">
+        <ButtonLink
+          href="https://supportbuy-platform.onrender.com/signin"
+          className=" flex text-base text-white font-bold justify-center items-center bg-sb-primary h-[34px] px-3.5 pb-1  rounded-[40px]"
+        >
+          Log in
+        </ButtonLink>
+        <ButtonLink
+          href="https://supportbuy-platform.onrender.com/signup?role=business"
+          className="flex text-base text-sb-primary  justify-center items-center  bg-sb-secondary h-[34px]  px-3.5  pb-1 rounded-[40px]"
+        >
+          List Your Business
+        </ButtonLink>
+      </div>
+    </nav>
+  );
 }
 
 export default BusinessTopNavBar;
